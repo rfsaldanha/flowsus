6b6630b862cf845ab1da035e4f9cacc5844a0217
Raphael Saldanha <rfsaldanha@gmail.com>
2024-06-06 15:20:54 +0200

    Update query

--- 02_query.R
+++ 02_query.R
@@ -1,12 +1,18 @@
 library(duckdb)
+library(duckplyr)
 
 con <- dbConnect(duckdb(), dbdir = "teste_pcdas_sih.duckdb", read_only = FALSE)
 
-query <- "SELECT cod_res, cod_int, COUNT(*) AS freq 
-FROM sih 
-WHERE dt_inter = '2014-12-01'
-AND def_ident = 'Normal' 
-AND def_car_int = 'Urgência'
-GROUP BY cod_res, cod_int"
+sih_tbl <- dplyr::tbl(con, "sih8")
 
-dbGetQuery(con, query)
+res <- sih_tbl |>
+  filter(def_ident == "Normal") |>
+  mutate(
+    year = year(dt_inter),
+    month = month(dt_inter)
+  ) |>
+  summarise(
+    freq = n(),
+    .by = c(year, month, cod_res, cod_int)
+  ) |>
+  collect()
