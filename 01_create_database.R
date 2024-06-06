63c63a5346d23fd4624f3b4aa450b8c50e46d3dd
Raphael Saldanha <rfsaldanha@gmail.com>
2024-06-06 14:53:45 +0200

    Create database script

---
+++ 01_create_database.R
@@ -0,0 +1,22 @@
+library(duckdb)
+
+con <- dbConnect(duckdb(), dbdir = "pcdas_sih.duckdb", read_only = FALSE)
+
+dbExecute(con, "CREATE TABLE sih AS 
+          SELECT res_codigo_adotado AS cod_res, 
+          int_MUNCOD AS cod_int, 
+          dt_inter, def_ident, def_car_int 
+          FROM read_csv('/media/raphael/lacie/pcdas_sih/csv/*.csv',
+          delim = ',',
+          header = true,
+          dateformat = '%Y%m%d',
+          types = {'res_codigo_adotado': 'VARCHAR',
+          'int_MUNCOD': 'VARCHAR',
+          'dt_inter': 'DATE',
+          'def_ident': 'VARCHAR',
+          'def_car_int': 'VARCHAR'}
+          )")
+
+dbGetQuery(con, "SELECT * FROM sih LIMIT 10")
+
+dbDisconnect(con)
